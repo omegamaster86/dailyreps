26434e1b3dc639e53f9d2a768829435b829bbae4
omegamaster86 <masya868686@gmail.com>
2025-01-02 14:42:26 +0900

    不要なドメインの削除

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,6 @@
 export default {
   providers: [
     {
-      // domain: "https://omegamaster86.clerk.dev",
       domain: "https://striking-ram-57.clerk.accounts.dev",
       applicationID: "convex"
     }
